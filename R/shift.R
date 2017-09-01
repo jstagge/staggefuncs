d260bc71ad1ebc07a52d813d8f011b9655782e8e
JamesStagge <jhstagge@gmail.com>
2017-08-31 20:59:24 -0600

    Added the shift function

---
+++ R/shift.R
@@ -0,0 +1,29 @@
+#' Shift a time series
+#'
+#' Copied from https://www.r-bloggers.com/generating-a-laglead-variables/
+#'
+#' @param x vector timeseries
+#' @param shift_by numeric number of spaces to shift. Can be positive or negative.
+#'
+#' @return out resulting numeric vector
+#'
+#'
+#' @export
+
+shift <-function(x,shift_by){
+    stopifnot(is.numeric(shift_by))
+    stopifnot(is.numeric(x))
+ 
+    if (length(shift_by)>1)
+        return(sapply(shift_by,shift, x=x))
+ 
+    out<-NULL
+    abs_shift_by=abs(shift_by)
+    if (shift_by > 0 )
+        out<-c(tail(x,-abs_shift_by),rep(NA,abs_shift_by))
+    else if (shift_by < 0 )
+        out<-c(rep(NA,abs_shift_by), head(x,-abs_shift_by))
+    else
+        out<-x
+    out
+}
